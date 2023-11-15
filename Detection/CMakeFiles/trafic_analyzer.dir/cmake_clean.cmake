920c315e14dec709c39ef9498cc0368ac0ab713d
Topor Denis <contact@topor.dev>
2023-11-15 16:00:44 +0200

    init

---
+++ Detection/CMakeFiles/trafic_analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trafic_analyzer.dir/src/main.cpp.o"
+  "CMakeFiles/trafic_analyzer.dir/src/main.cpp.o.d"
+  "trafic_analyzer"
+  "trafic_analyzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trafic_analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Detection/src/main.cpp
@@ -0,0 +1,154 @@
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
+
+std::vector<std::string> load_class_list()
+{
+    std::vector<std::string> class_list;
+    std::ifstream ifs("config_files/classes.txt");
+    std::string line;
+    while (getline(ifs, line))
+    {
+        class_list.push_back(line);
+    }
+    return class_list;
+}
+
+const std::string YOLO_VERSION = "v4-tiny";
+
+void load_net(cv::dnn::Net &net, bool is_cuda)
+{
+    auto result = cv::dnn::readNetFromDarknet("config_files/yolo" + YOLO_VERSION + ".cfg", "config_files/yolo" + YOLO_VERSION + ".weights");
+    if (is_cuda)
+    {
+        std::cout << "Attempty to use CUDA\n";
+        result.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+        result.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA_FP16);
+    }
+    else
+    {
+        std::cout << "Running on CPU\n";
+        result.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+        result.setPreferableTarget(cv::dnn::DNN_TARGET_OPENCL);
+    }
+    net = result;
+}
+
+const std::vector<cv::Scalar> colors = {cv::Scalar(255, 255, 0), cv::Scalar(0, 255, 0), cv::Scalar(0, 255, 255), cv::Scalar(255, 0, 0)};
+
+int main(int argc, char **argv)
+{
+    cv::ocl::setUseOpenCL(true);
+    if (!cv::ocl::haveOpenCL())
+    {
+        std::cout << "OpenCL is not avaiable..." << std::endl;
+        return 0;
+    }
+    cv::ocl::Context context;
+    if (!context.create(cv::ocl::Device::TYPE_GPU))
+    {
+        std::cout << "Failed creating the context..." << std::endl;
+        return 0;
+    }
+    // In OpenCV 3.0.0 beta, only a single device is detected.
+    std::cout << context.ndevices() << " GPU devices are detected." << std::endl;
+    for (int i = 0; i < context.ndevices(); i++)
+    {
+        cv::ocl::Device device = context.device(i);
+        std::cout << "name                 : " << device.name() << std::endl;
+        std::cout << "available            : " << device.available() << std::endl;
+        std::cout << "imageSupport         : " << device.imageSupport() << std::endl;
+        std::cout << "OpenCL_C_Version     : " << device.OpenCL_C_Version() << std::endl;
+        std::cout << std::endl;
+    }
+    cv::ocl::Device(context.device(0));
+
+    std::vector<std::string> class_list = load_class_list();
+
+    cv::Mat frame;
+    cv::VideoCapture capture("sample.mp4");
+    if (!capture.isOpened())
+    {
+        std::cerr << "Error opening video file\n";
+        return -1;
+    }
+
+    bool is_cuda = argc > 1 && strcmp(argv[1], "cuda") == 0;
+
+    cv::dnn::Net net;
+    load_net(net, is_cuda);
+
+    auto model = cv::dnn::DetectionModel(net);
+    model.setInputParams(1. / 255, cv::Size(416, 416), cv::Scalar(), true);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    int frame_count = 0;
+    float fps = -1;
+    int total_frames = 0;
+
+    while (true)
+    {
+        capture.read(frame);
+        if (frame.empty())
+        {
+            std::cout << "End of stream\n";
+            break;
+        }
+
+        std::vector<int> classIds;
+        std::vector<float> confidences;
+        std::vector<cv::Rect> boxes;
+        model.detect(frame, classIds, confidences, boxes, .2, .4);
+        frame_count++;
+        total_frames++;
+
+        int detections = classIds.size();
+
+        for (int i = 0; i < detections; ++i)
+        {
+
+            auto box = boxes[i];
+            auto classId = classIds[i];
+            const auto color = colors[classId % colors.size()];
+            cv::rectangle(frame, box, color, 3);
+
+            cv::rectangle(frame, cv::Point(box.x, box.y - 20), cv::Point(box.x + box.width, box.y), color, cv::FILLED);
+            cv::putText(frame, class_list[classId].c_str(), cv::Point(box.x, box.y - 5), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        }
+
+        if (frame_count >= 30)
+        {
+
+            auto end = std::chrono::high_resolution_clock::now();
+            fps = frame_count * 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+            frame_count = 0;
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        if (fps > 0)
+        {
+
+            std::ostringstream fps_label;
+            fps_label << std::fixed << std::setprecision(2);
+            fps_label << "FPS: " << fps;
+            std::string fps_label_str = fps_label.str();
+
+            cv::putText(frame, fps_label_str.c_str(), cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+        }
+
+        cv::imshow("output", frame);
+
+        if (cv::waitKey(1) != -1)
+        {
+            capture.release();
+            std::cout << "finished by user\n";
+            break;
+        }
+    }
+
+    std::cout << "Total frames: " << total_frames << "\n";
+
+    return 0;
+}
